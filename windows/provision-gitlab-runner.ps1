9dd8f1efd8ee52b280f378844fd0b99659357e1a
Rui Lopes <rgl@ruilopes.com>
2021-04-26 08:52:38 +0100

    upgrade to gitlab runner 13.11.0

--- windows/provision-gitlab-runner.ps1
+++ windows/provision-gitlab-runner.ps1
@@ -6,7 +6,7 @@ param(
     [string]$config_fqdn = 'windows.gitlab.example.com',
 
     [Parameter(Mandatory=$true)]
-    [string]$gitlabRunnerVersion = '13.10.0'
+    [string]$gitlabRunnerVersion = '13.11.0'
 )
 
 $config_gitlab_ip = "$((Resolve-DNSName $config_gitlab_fqdn).IPAddress)"
