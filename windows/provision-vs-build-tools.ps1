d140d4a1f3c59ab3b27ac091f3f0a63d043afcee
Rui Lopes <rgl@ruilopes.com>
2021-06-12 12:40:19 +0100

    upgrade to vs build tools 16.10.1

--- windows/provision-vs-build-tools.ps1
+++ windows/provision-vs-build-tools.ps1
@@ -10,15 +10,15 @@ choco install -y netfx-4.5.2-devpack
 # add support for building applications that target the .net 4.8 framework.
 choco install -y netfx-4.8-devpack
 
-# install the Visual Studio Build Tools 2019 16.9.4.
+# install the Visual Studio Build Tools 2019 16.10.1.
 # see https://www.visualstudio.com/downloads/
 # see https://docs.microsoft.com/en-us/visualstudio/releases/2019/release-notes
 # see https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2019
 # see https://docs.microsoft.com/en-us/visualstudio/install/command-line-parameter-examples?view=vs-2019
 # see https://docs.microsoft.com/en-us/visualstudio/install/workload-and-component-ids?view=vs-2019
 # see https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools?view=vs-2019
-$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/3105fcfe-e771-41d6-9a1c-fc971e7d03a7/67584dbce4956291c76a5deabbadc15c9ad6a50d6a5f4458765b6bd3a924aead/vs_BuildTools.exe'
-$archiveHash = '67584dbce4956291c76a5deabbadc15c9ad6a50d6a5f4458765b6bd3a924aead'
+$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/5c44c598-f77e-4815-89ca-e7a1f87c579a/b9cf9816d67a8f1acaf811afb5a11f195456a18c7b42d0901ddf1d322324c82c/vs_BuildTools.exe'
+$archiveHash = 'b9cf9816d67a8f1acaf811afb5a11f195456a18c7b42d0901ddf1d322324c82c'
 $archiveName = Split-Path $archiveUrl -Leaf
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host 'Downloading the Visual Studio Build Tools Setup Bootstrapper...'
