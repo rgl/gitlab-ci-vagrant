966c313c26b3193a9f15a6035650de1017ddf8bd
Rui Lopes <rgl@ruilopes.com>
2021-12-26 12:04:05 +0000

    install report generator

--- windows/configure-gitlab-runner-home.ps1
+++ windows/configure-gitlab-runner-home.ps1
@@ -13,8 +13,16 @@ trap {
 # install the sourcelink dotnet global tool.
 # NB this is installed at %USERPROFILE%\.dotnet\tools.
 # see https://github.com/dotnet/sourcelink
-dotnet tool install --global sourcelink
+# see https://github.com/ctaggart/SourceLink
+# see https://www.nuget.org/packages/SourceLink
+dotnet tool install --global SourceLink --version 3.1.1
 
 # install the xUnit to JUnit report converter.
 # see https://github.com/gabrielweyer/xunit-to-junit
-dotnet tool install --global dotnet-xunit-to-junit
+# see https://www.nuget.org/packages/dotnet-xunit-to-junit
+dotnet tool install --global dotnet-xunit-to-junit --version 3.0.2
+
+# install the report generator.
+# see https://github.com/danielpalme/ReportGenerator
+# see https://www.nuget.org/packages/dotnet-reportgenerator-globaltool
+dotnet tool install --global dotnet-reportgenerator-globaltool --version 5.0.0
