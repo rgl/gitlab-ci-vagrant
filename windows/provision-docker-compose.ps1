4080ebe3c09a531462415bcfddc2e331c3a1ccc7
Rui Lopes <rgl@ruilopes.com>
2022-03-04 17:48:15 +0000

    add link to upstream

--- windows/provision-docker-compose.ps1
+++ windows/provision-docker-compose.ps1
@@ -1,2 +1,4 @@
+# see https://community.chocolatey.org/packages/docker-compose
+# see https://github.com/docker/compose
 choco install -y --version 1.29.2 docker-compose
 docker-compose --version
--- windows/provision-dotnet.ps1
+++ windows/provision-dotnet.ps1
@@ -1,1 +1,2 @@
+# see https://community.chocolatey.org/packages/dotnetfx
 Start-Choco install,-y,dotnetfx,--version=4.8.0.20190930 @(0,3010)
