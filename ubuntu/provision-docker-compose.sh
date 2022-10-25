cfe903e1bef21ecf4979b7dd8ac9c421bb181498
Rui Lopes <rgl@ruilopes.com>
2022-10-25 06:45:55 +0100

    upgrade to docker compose 2.12.2

--- ubuntu/provision-docker-compose.sh
+++ ubuntu/provision-docker-compose.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 # download.
 # see https://docs.docker.com/compose/install/#install-compose-on-linux-systems
 # see https://github.com/docker/compose/releases
-docker_compose_version='2.11.2'
+docker_compose_version='2.12.2'
 docker_compose_url="https://github.com/docker/compose/releases/download/v$docker_compose_version/docker-compose-linux-$(uname -m)"
 wget -qO /tmp/docker-compose "$docker_compose_url"
 
--- windows/provision-docker-compose.ps1
+++ windows/provision-docker-compose.ps1
@@ -1,9 +1,9 @@
 # download install the docker-compose binaries.
 # see https://github.com/docker/compose/releases
-$archiveVersion = '2.11.2'
+$archiveVersion = '2.12.2'
 $archiveUrl = "https://github.com/docker/compose/releases/download/v$archiveVersion/docker-compose-windows-x86_64.exe"
 $archiveName = Split-Path -Leaf $archiveUrl
-$archiveHash = '32172ee7ddc9791e9d5f7e7f2e1467c59c3bdb8cd2eb625b3e2fda38b76c9ab7'
+$archiveHash = '0356b20170258be37295774b89a32b294dd524b646ec6df896026a82c43d6f62'
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host "Installing docker-compose $archiveVersion..."
 (New-Object System.Net.WebClient).DownloadFile($archiveUrl, $archivePath)
