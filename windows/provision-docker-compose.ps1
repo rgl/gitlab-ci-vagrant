5d4b063f9dac2eb9d2da131633b9cdeabd39b04c
Rui Lopes <rgl@ruilopes.com>
2022-12-01 10:09:08 +0000

    upgrade to docker compose 2.13.0

--- windows/provision-docker-compose.ps1
+++ windows/provision-docker-compose.ps1
@@ -1,9 +1,9 @@
 # download install the docker-compose binaries.
 # see https://github.com/docker/compose/releases
-$archiveVersion = '2.12.2'
+$archiveVersion = '2.13.0'
 $archiveUrl = "https://github.com/docker/compose/releases/download/v$archiveVersion/docker-compose-windows-x86_64.exe"
 $archiveName = Split-Path -Leaf $archiveUrl
-$archiveHash = '0356b20170258be37295774b89a32b294dd524b646ec6df896026a82c43d6f62'
+$archiveHash = '09f099b74dcb566a5d77c46d3e8e6061640bbec3c6f329c7863040a933d2d358'
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host "Installing docker-compose $archiveVersion..."
 (New-Object System.Net.WebClient).DownloadFile($archiveUrl, $archivePath)
