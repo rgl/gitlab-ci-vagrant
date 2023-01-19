3ebb9e73567b48703c53de9b104b6029efa81b4c
Rui Lopes <rgl@ruilopes.com>
2023-01-19 17:49:25 +0000

    ensure that all the docker plugins inherit the parent directory permissions

--- windows/provision-docker-compose.ps1
+++ windows/provision-docker-compose.ps1
@@ -15,4 +15,11 @@ if ($archiveActualHash -ne $archiveHash) {
 $dockerCliPluginsPath = "$env:ProgramData\docker\cli-plugins"
 mkdir -Force $dockerCliPluginsPath | Out-Null
 Move-Item -Force $archivePath "$dockerCliPluginsPath\docker-compose.exe"
+# ensure that all the plugins inherit the parent directory permissions.
+# NB this ensures that any user can execute the plugins.
+Import-Module Carbon
+Get-ChildItem -Recurse $dockerCliPluginsPath | ForEach-Object {
+    Enable-CAclInheritance $_.FullName
+}
+# try the binary.
 docker compose version
