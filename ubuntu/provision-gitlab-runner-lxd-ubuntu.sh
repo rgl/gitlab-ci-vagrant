2a53bddcfe9789b35dc72097c06bcb38cf6238d1
Rui Lopes <rgl@ruilopes.com>
2023-02-03 19:15:00 +0000

    upgade to gitlab-runner 15.8.1

--- ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
+++ ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-gitlab_runner_version="${1:-15.8.0}"; shift || true
+gitlab_runner_version="${1:-15.8.1}"; shift || true
 os_name="$(lsb_release -si)"
 os_version="$(lsb_release -sr)"
 os_codename="$(lsb_release -sc)"
