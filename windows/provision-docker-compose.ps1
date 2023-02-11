3133fec6d4b2e60985ffcef456bb71b428468625
Rui Lopes <rgl@ruilopes.com>
2023-02-11 10:26:20 +0000

    upgrade to docker compose 2.16.0

--- windows/provision-docker-compose.ps1
+++ windows/provision-docker-compose.ps1
@@ -1,17 +1,12 @@
 # download install the docker-compose binaries.
 # see https://github.com/docker/compose/releases
 # renovate: datasource=github-releases depName=docker/compose
-$archiveVersion = '2.15.1'
+$archiveVersion = '2.16.0'
 $archiveUrl = "https://github.com/docker/compose/releases/download/v$archiveVersion/docker-compose-windows-x86_64.exe"
 $archiveName = Split-Path -Leaf $archiveUrl
-$archiveHash = '538598025dc9ae41298abf859d5b312fba40359c691d9b14527375f4633cbe65'
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host "Installing docker-compose $archiveVersion..."
 (New-Object System.Net.WebClient).DownloadFile($archiveUrl, $archivePath)
-$archiveActualHash = (Get-FileHash $archivePath -Algorithm SHA256).Hash
-if ($archiveActualHash -ne $archiveHash) {
-    throw "the $archiveUrl file hash $archiveActualHash does not match the expected $archiveHash"
-}
 $dockerCliPluginsPath = "$env:ProgramData\docker\cli-plugins"
 mkdir -Force $dockerCliPluginsPath | Out-Null
 Move-Item -Force $archivePath "$dockerCliPluginsPath\docker-compose.exe"
