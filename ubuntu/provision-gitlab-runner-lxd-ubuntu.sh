28f20cfd969107e2007b55b41bb15a6956f0df66
Rui Lopes <rgl@ruilopes.com>
2023-03-28 19:58:03 +0100

    upgrade to docker 23.0.2

--- ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
+++ ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 gitlab_runner_version="${1:-15.9.1}"; shift || true
-docker_version="${1:-23.0.1}"; shift || true
+docker_version="${1:-23.0.2}"; shift || true
 os_name="$(lsb_release -si)"
 os_version="$(lsb_release -sr)"
 os_codename="$(lsb_release -sc)"
