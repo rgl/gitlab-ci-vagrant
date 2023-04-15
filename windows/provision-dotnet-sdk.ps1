71f0aef4e512a7468faf905c319167c9ae1db338
Rui Lopes <rgl@ruilopes.com>
2023-04-15 12:30:04 +0100

    upgrade to dotnet-sdk 6.0.408 (dotnet 6.0.16)

--- windows/provision-dotnet-sdk.ps1
+++ windows/provision-dotnet-sdk.ps1
@@ -1,13 +1,13 @@
 # see https://dotnet.microsoft.com/en-us/download/dotnet/6.0
-# see https://github.com/dotnet/core/blob/main/release-notes/6.0/6.0.15/6.0.15.md
+# see https://github.com/dotnet/core/blob/main/release-notes/6.0/6.0.16/6.0.16.md
 
 # opt-out from dotnet telemetry.
 [Environment]::SetEnvironmentVariable('DOTNET_CLI_TELEMETRY_OPTOUT', '1', 'Machine')
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 
 # install the dotnet sdk.
-$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/38dca5f5-f10f-49fb-b07f-a42dd123ea30/335bb4811c9636b3a4687757f9234db9/dotnet-sdk-6.0.407-win-x64.exe'
-$archiveHash = '80b05ac0009ca99a690edda4c4ba59b66e1025aa94a002638317f3aa9007a993e27a30d1f48ebbdc84b9ff511535745021550bc759dfaf2ef4c48fbf555682ea'
+$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/df42b901-8ce2-4131-941a-b3fa094ff3d8/556da65f7a2f6164bf3df932e030898a/dotnet-sdk-6.0.408-win-x64.exe'
+$archiveHash = '11b65567cdf90c177deef4d1762e193f7c3eb3d1d6758b0bf8c250e2338ac44421043f0eb7c3690354120a5af1c6a1685ac3fc4888aea23a5de95bca9a2248a2'
 $archiveName = Split-Path -Leaf $archiveUrl
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host "Downloading $archiveName..."
