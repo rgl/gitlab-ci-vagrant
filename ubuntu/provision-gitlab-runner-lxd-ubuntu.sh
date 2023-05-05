814fdacbbc88bfe5bdd0b31e2b515ae1bc52e5de
Rui Lopes <rgl@ruilopes.com>
2023-05-05 12:33:40 +0100

    upgrade to gitlab-runner 15.11.0

--- ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
+++ ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-gitlab_runner_version="${1:-15.10.1}"; shift || true
+gitlab_runner_version="${1:-15.11.0}"; shift || true
 docker_version="${1:-23.0.5}"; shift || true
 os_name="$(lsb_release -si)"
 os_version="$(lsb_release -sr)"
