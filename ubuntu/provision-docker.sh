317de545b08377773016a35e9c024933bef6c5ff
Rui Lopes <rgl@ruilopes.com>
2023-05-26 17:23:10 +0100

    upgrade to docker 24.0.2

--- ubuntu/provision-docker.sh
+++ ubuntu/provision-docker.sh
@@ -3,7 +3,7 @@ set -eux
 
 # see https://github.com/moby/moby/releases
 # renovate: datasource=github-releases depName=moby/moby
-docker_version='24.0.1'
+docker_version='24.0.2'
 
 # prevent apt-get et al from asking questions.
 # NB even with this, you'll still get some warnings that you can ignore:
