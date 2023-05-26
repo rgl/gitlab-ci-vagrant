d1959ea9ff0ad2b5670594843dfad303d39e5028
Rui Lopes <rgl@ruilopes.com>
2023-05-26 17:22:32 +0100

    upgrade to gitlab-runner 16.0.1

--- windows/provision-gitlab-runner.ps1
+++ windows/provision-gitlab-runner.ps1
@@ -6,7 +6,7 @@ param(
     [string]$config_fqdn = 'windows.gitlab.example.com',
 
     [Parameter(Mandatory=$true)]
-    [string]$gitlabRunnerVersion = '15.11.0'
+    [string]$gitlabRunnerVersion = '16.0.1'
 )
 
 $config_gitlab_ip = "$((Resolve-DNSName $config_gitlab_fqdn).IPAddress)"
