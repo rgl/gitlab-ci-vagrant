5de30a1aea2ced9fe6fa40e8f9d5ea78d3d6f4b1
Rui Lopes <rgl@ruilopes.com>
2023-11-16 18:25:30 +0000

    upgrade to visual studio build tools 17.8

--- windows/provision-vs-build-tools.ps1
+++ windows/provision-vs-build-tools.ps1
@@ -1,7 +1,7 @@
 # add support for building applications that target the .net 4.8 framework.
 choco install -y netfx-4.8-devpack
 
-# install the Visual Studio Build Tools 2022 17.6.7.
+# install the Visual Studio Build Tools 2022 17.8.
 # see https://learn.microsoft.com/en-us/visualstudio/releases/2022/release-history#fixed-version-bootstrappers
 # see https://learn.microsoft.com/en-us/visualstudio/releases/2022/release-notes
 # see https://learn.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2022
@@ -9,8 +9,8 @@ choco install -y netfx-4.8-devpack
 # see https://learn.microsoft.com/en-us/visualstudio/install/workload-and-component-ids?view=vs-2022
 # see https://learn.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools?view=vs-2022
 # NB update the windbg version in provision-procdump-as-postmortem-debugger.ps1 to match the installed Windows10SDK.19041.
-$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/27b00650-dd0d-4ee7-a7f9-cc469ca32da4/1aab3100e7e7ae01f2688a9369f861d7e475c54ccc9d0558a6723c3ede0160b8/vs_BuildTools.exe'
-$archiveHash = '1aab3100e7e7ae01f2688a9369f861d7e475c54ccc9d0558a6723c3ede0160b8'
+$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/eb105084-8c42-4491-a292-51b4ab48d847/8e07efcfc41db883fa4e8136c9bfb572aadfcc98512c205a7b9e43564ee404ff/vs_BuildTools.exe'
+$archiveHash = '8e07efcfc41db883fa4e8136c9bfb572aadfcc98512c205a7b9e43564ee404ff'
 $archiveName = Split-Path $archiveUrl -Leaf
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host 'Downloading the Visual Studio Build Tools Setup Bootstrapper...'
