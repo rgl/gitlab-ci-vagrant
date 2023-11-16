2d147b37da39c635617fb50df213dc43e7071bc3
Rui Lopes <rgl@ruilopes.com>
2023-11-16 18:20:25 +0000

    upgrade to docker 24.0.7

--- ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
+++ ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 gitlab_runner_version="${1:-16.4.0}"; shift || true
-docker_version="${1:-24.0.6}"; shift || true
+docker_version="${1:-24.0.7}"; shift || true
 os_name="$(lsb_release -si)"
 os_version="$(lsb_release -sr)"
 os_codename="$(lsb_release -sc)"
--- windows/provision-docker-ce.ps1
+++ windows/provision-docker-ce.ps1
@@ -1,11 +1,11 @@
 # see https://learn.microsoft.com/en-us/virtualization/windowscontainers/manage-docker/configure-docker-daemon
 # see https://docs.docker.com/engine/installation/linux/docker-ce/binaries/#install-server-and-client-binaries-on-windows
-# see https://github.com/moby/moby/releases/tag/v24.0.6
-# see https://github.com/rgl/docker-ce-windows-binaries-vagrant/releases/tag/v24.0.6
+# see https://github.com/moby/moby/releases/tag/v24.0.7
+# see https://github.com/rgl/docker-ce-windows-binaries-vagrant/releases/tag/v24.0.7
 
 # download install the docker binaries.
 # renovate: datasource=github-releases depName=rgl/docker-ce-windows-binaries-vagrant
-$archiveVersion = '24.0.6'
+$archiveVersion = '24.0.7'
 $archiveName = "docker-$archiveVersion.zip"
 $archiveUrl = "https://github.com/rgl/docker-ce-windows-binaries-vagrant/releases/download/v$archiveVersion/$archiveName"
 $archivePath = "$env:TEMP\$archiveName"
