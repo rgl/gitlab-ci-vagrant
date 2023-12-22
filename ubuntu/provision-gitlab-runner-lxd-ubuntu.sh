6a28924b127a633dbd3951f3847819896d5d6569
Rui Lopes <rgl@ruilopes.com>
2023-12-22 07:45:30 +0000

    upgrade to gitlab-runner 16.7.0

--- ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
+++ ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-gitlab_runner_version="${1:-16.6.1}"; shift || true
+gitlab_runner_version="${1:-16.7.0}"; shift || true
 docker_version="${1:-24.0.7}"; shift || true
 os_name="$(lsb_release -si)"
 os_version="$(lsb_release -sr)"
