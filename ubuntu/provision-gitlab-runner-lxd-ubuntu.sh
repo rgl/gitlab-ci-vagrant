aa3fd828b1aa36f72732b3280838111d52499581
Rui Lopes <rgl@ruilopes.com>
2024-05-26 17:42:54 +0100

    upgrade to docker compose 2.27.1

--- ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
+++ ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 gitlab_runner_version="${1:-16.11.1}"; shift || true
 docker_version="${1:-26.1.3}"; shift || true
-docker_compose_version="${1:-2.27.0}"; shift || true
+docker_compose_version="${1:-2.27.1}"; shift || true
 os_name="$(lsb_release -si)"
 os_version="$(lsb_release -sr)"
 os_codename="$(lsb_release -sc)"
