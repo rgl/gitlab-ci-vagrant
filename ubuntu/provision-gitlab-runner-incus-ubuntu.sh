8d928993eec2b6dcddea47e4de0dd034a335027d
Rui Lopes <rgl@ruilopes.com>
2024-07-28 09:05:42 +0100

    upgrade to docker 27.1.1

--- ubuntu/provision-gitlab-runner-incus-ubuntu.sh
+++ ubuntu/provision-gitlab-runner-incus-ubuntu.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 gitlab_runner_version="${1:-17.2.0}"; shift || true
-docker_version="${1:-27.0.3}"; shift || true
+docker_version="${1:-27.1.1}"; shift || true
 docker_compose_version="${1:-2.29.0}"; shift || true
 os_name="$(lsb_release -si)"
 os_version="$(lsb_release -sr)"
