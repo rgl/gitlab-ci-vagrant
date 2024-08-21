2380e89127615ee6218bd91229e3b722f43ba061
Rui Lopes <rgl@ruilopes.com>
2024-08-21 07:56:59 +0100

    upgrade to gitlab-runner 17.3.0

--- ubuntu/provision-gitlab-runner-incus-ubuntu.sh
+++ ubuntu/provision-gitlab-runner-incus-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-gitlab_runner_version="${1:-17.2.1}"; shift || true
+gitlab_runner_version="${1:-17.3.0}"; shift || true
 docker_version="${1:-27.1.2}"; shift || true
 docker_compose_version="${1:-2.29.2}"; shift || true
 os_name="$(lsb_release -si)"
--- windows/provision-gitlab-runner.ps1
+++ windows/provision-gitlab-runner.ps1
@@ -6,7 +6,7 @@ param(
     [string]$config_fqdn = 'windows.gitlab.example.com',
 
     [Parameter(Mandatory=$true)]
-    [string]$gitlabRunnerVersion = '17.2.1'
+    [string]$gitlabRunnerVersion = '17.3.0'
 )
 
 $config_gitlab_ip = "$((Resolve-DNSName $config_gitlab_fqdn).IPAddress)"
