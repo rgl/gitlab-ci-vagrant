73e9fc81a2f68b6973651eeb9c54c5c6e1a2a2c1
Rui Lopes <rgl@ruilopes.com>
2024-09-20 07:49:48 +0100

    upgrade to gitlab-runner 17.4.0

--- ubuntu/provision-gitlab-runner-incus-ubuntu.sh
+++ ubuntu/provision-gitlab-runner-incus-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-gitlab_runner_version="${1:-17.3.0}"; shift || true
+gitlab_runner_version="${1:-17.4.0}"; shift || true
 docker_version="${1:-27.2.1}"; shift || true
 docker_compose_version="${1:-2.29.6}"; shift || true
 os_name="$(lsb_release -si)"
