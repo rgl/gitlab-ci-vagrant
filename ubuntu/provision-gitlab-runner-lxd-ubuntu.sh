112f03479769f630f4ec1473420f73aada4118ff
Rui Lopes <rgl@ruilopes.com>
2025-01-11 15:48:05 +0000

    upgrade to docker 27.4.1

--- ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
+++ ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 gitlab_runner_version="${1:-17.5.1}"; shift || true
-docker_version="${1:-27.3.1}"; shift || true
+docker_version="${1:-27.4.1}"; shift || true
 docker_compose_version="${1:-2.29.7}"; shift || true
 os_name="$(lsb_release -si)"
 os_version="$(lsb_release -sr)"
