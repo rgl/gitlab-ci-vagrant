6bf7245db04b09bcbb1397e3617a566eb4f75086
Rui Lopes <rgl@ruilopes.com>
2025-01-17 08:53:35 +0000

    upgrade to docker compose 2.32.4

--- ubuntu/provision-gitlab-runner-incus-ubuntu.sh
+++ ubuntu/provision-gitlab-runner-incus-ubuntu.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 gitlab_runner_version="${1:-17.7.0}"; shift || true
 docker_version="${1:-27.5.0}"; shift || true
-docker_compose_version="${1:-2.32.2}"; shift || true
+docker_compose_version="${1:-2.32.4}"; shift || true
 os_name="$(lsb_release -si)"
 os_version="$(lsb_release -sr)"
 os_codename="$(lsb_release -sc)"
