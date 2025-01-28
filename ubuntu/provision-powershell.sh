0321615e2a769ac0c3aa1be07cec05dd087e63b6
Rui Lopes <rgl@ruilopes.com>
2025-01-28 08:36:04 +0000

    upgrade to powershell 7.4.7

--- ubuntu/provision-powershell.sh
+++ ubuntu/provision-powershell.sh
@@ -10,7 +10,7 @@ source /etc/profile.d/opt-out-powershell-telemetry.sh
 # see https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu?view=powershell-7.4
 # see https://github.com/PowerShell/PowerShell/releases
 # renovate: datasource=github-releases depName=PowerShell/PowerShell extractVersion=^v(?<version>7\.4\..+)
-powershell_version='7.4.6'
+powershell_version='7.4.7'
 wget -qO packages-microsoft-prod.deb "https://packages.microsoft.com/config/ubuntu/$(lsb_release -s -r)/packages-microsoft-prod.deb"
 dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
