6688f7e8589fb8ca6c0d6ac57f310bde091c05bc
Rui Lopes <rgl@ruilopes.com>
2025-04-06 22:11:23 +0000

    upgrade to gitlab-runner 17.10.1

--- ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
+++ ubuntu/provision-gitlab-runner-lxd-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-gitlab_runner_version="${1:-17.9.0}"; shift || true
+gitlab_runner_version="${1:-17.10.1}"; shift || true
 docker_version="${1:-28.0.4}"; shift || true
 docker_compose_version="${1:-2.34.0}"; shift || true
 os_name="$(lsb_release -si)"
--- ubuntu/provision-gitlab-runner.sh
+++ ubuntu/provision-gitlab-runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-gitlab_runner_version="${1:-17.9.0}"; shift || true
+gitlab_runner_version="${1:-17.10.1}"; shift || true
 os_name="$(lsb_release -si)"
 os_version="$(lsb_release -sr)"
 config_gitlab_fqdn=$(hostname --domain)
