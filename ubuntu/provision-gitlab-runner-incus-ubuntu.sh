aa5eee840034d46ac380e0d327ed93ba2c055c3e
Rui Lopes <rgl@ruilopes.com>
2025-08-25 23:22:12 +0000

    upgrade to gitlab-runner 18.3.0

--- ubuntu/provision-gitlab-runner-incus-ubuntu.sh
+++ ubuntu/provision-gitlab-runner-incus-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-gitlab_runner_version="${1:-18.2.1}"; shift || true
+gitlab_runner_version="${1:-18.3.0}"; shift || true
 docker_version="${1:-28.3.3}"; shift || true
 docker_compose_version="${1:-2.39.2}"; shift || true
 os_name="$(lsb_release -si)"
