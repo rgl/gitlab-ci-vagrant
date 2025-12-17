1d35488eaf084ad900ff39b835727c88e2e58d05
Rui Lopes <rgl@ruilopes.com>
2025-12-17 08:00:00 +0000

    upgrade to visual studio build tools 18.1.0

--- windows/provision-vs-build-tools.ps1
+++ windows/provision-vs-build-tools.ps1
@@ -1,16 +1,16 @@
 # add support for building applications that target the .net 4.8 framework.
 choco install -y netfx-4.8-devpack
 
-# install the Visual Studio Build Tools 2022 17.12.13.
-# see https://learn.microsoft.com/en-us/visualstudio/releases/2022/release-history#fixed-version-bootstrappers
-# see https://learn.microsoft.com/en-us/visualstudio/releases/2022/release-notes
-# see https://learn.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2022
-# see https://learn.microsoft.com/en-us/visualstudio/install/command-line-parameter-examples?view=vs-2022
-# see https://learn.microsoft.com/en-us/visualstudio/install/workload-and-component-ids?view=vs-2022
-# see https://learn.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools?view=vs-2022
-# NB update the windbg version in provision-procdump-as-postmortem-debugger.ps1 to match the installed Windows10SDK.19041.
-$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/a5f47ff8-ce7c-40ca-9680-001a0bdf0a11/662a2d989b56cce5ff88da7ebbe353c288a1dd3ddc4d2977a9b5d2e6e0d69a19/vs_BuildTools.exe'
-$archiveHash = '662a2d989b56cce5ff88da7ebbe353c288a1dd3ddc4d2977a9b5d2e6e0d69a19'
+# install the Visual Studio Build Tools 2026 18.1.0.
+# see https://learn.microsoft.com/en-us/visualstudio/releases/2026/release-history#release-dates-and-build-numbers
+# see https://learn.microsoft.com/en-us/visualstudio/releases/2026/release-notes
+# see https://learn.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=visualstudio
+# see https://learn.microsoft.com/en-us/visualstudio/install/command-line-parameter-examples?view=visualstudio
+# see https://learn.microsoft.com/en-us/visualstudio/install/workload-and-component-ids?view=visualstudio
+# see https://learn.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools?view=visualstudio
+# NB update the windbg version in provision-procdump-as-postmortem-debugger.ps1 to match the installed Windows11SDK.26100.
+$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/451b234a-4e25-491d-a007-bf3e55b2562f/442956195fde7b7a0be755d2dc1bc405c05b80115f26bac3b569cb0c358b303f/vs_BuildTools.exe'
+$archiveHash = '442956195fde7b7a0be755d2dc1bc405c05b80115f26bac3b569cb0c358b303f'
 $archiveName = Split-Path $archiveUrl -Leaf
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host 'Downloading the Visual Studio Build Tools Setup Bootstrapper...'
@@ -20,7 +20,7 @@ if ($archiveHash -ne $archiveActualHash) {
     throw "$archiveName downloaded from $archiveUrl to $archivePath has $archiveActualHash hash witch does not match the expected $archiveHash"
 }
 Write-Host 'Installing the Visual Studio Build Tools...'
-$vsBuildToolsHome = 'C:\VS2022BuildTools'
+$vsBuildToolsHome = 'C:\VS2026BuildTools'
 for ($try = 1; ; ++$try) {
     &$archivePath `
         --installPath $vsBuildToolsHome `
@@ -28,7 +28,7 @@ for ($try = 1; ; ++$try) {
         --add Microsoft.VisualStudio.Workload.VCTools `
         --add Microsoft.VisualStudio.Component.VC.CLI.Support `
         --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 `
-        --add Microsoft.VisualStudio.Component.Windows10SDK.19041 `
+        --add Microsoft.VisualStudio.Component.Windows11SDK.26100 `
         --norestart `
         --quiet `
         --wait `
