3fe68148b1795ef84818fb398b13811f699ac619
Rui Lopes <rgl@ruilopes.com>
2026-03-18 07:30:18 +0000

    upgrade to dotnet sdk 10.0.201 (dotnet 10.0.5)

--- windows/provision-dotnet-sdk.ps1
+++ windows/provision-dotnet-sdk.ps1
@@ -1,13 +1,13 @@
 # see https://dotnet.microsoft.com/en-us/download/dotnet/10.0
-# see https://github.com/dotnet/core/blob/main/release-notes/10.0/10.0.3/10.0.3.md
+# see https://github.com/dotnet/core/blob/main/release-notes/10.0/10.0.5/10.0.5.md
 
 # opt-out from dotnet telemetry.
 [Environment]::SetEnvironmentVariable('DOTNET_CLI_TELEMETRY_OPTOUT', '1', 'Machine')
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 
 # install the dotnet sdk.
-$archiveUrl = 'https://builds.dotnet.microsoft.com/dotnet/Sdk/10.0.103/dotnet-sdk-10.0.103-win-x64.exe'
-$archiveHash = '08dc7d3cceb3ce45be05d0bea55d5f6d3378cc2641e69fe499b153ad361f0e99f8b87daf0ec93a570ef93459f8f04bac63ea6c65633e919ff24e4c936e1c6eaa'
+$archiveUrl = 'https://builds.dotnet.microsoft.com/dotnet/Sdk/10.0.201/dotnet-sdk-10.0.201-win-x64.exe'
+$archiveHash = '4f51c56e4e9976266f8f6a44e40583bf942955a192a88ceb482fba7975a1eb8f049ae2dc6265c0b3972f1e1fea7237cc96fea46c38c0fa9c29b0b51e6aa414e4'
 $archiveName = Split-Path -Leaf $archiveUrl
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host "Downloading $archiveName..."
