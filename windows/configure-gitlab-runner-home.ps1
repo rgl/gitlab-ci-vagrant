02146bcbe12b8f11594ce1abd4ced05426cb0cb7
Rui Lopes <rgl@ruilopes.com>
2026-03-18 07:30:18 +0000

    upgrade to dotnet-reportgenerator-globaltool 5.5.3

--- windows/configure-gitlab-runner-home.ps1
+++ windows/configure-gitlab-runner-home.ps1
@@ -45,5 +45,5 @@ dotnet tool install --global dotnet-xunit-to-junit --version $dotnetXunitToJunit
 # see https://github.com/danielpalme/ReportGenerator
 # see https://www.nuget.org/packages/dotnet-reportgenerator-globaltool
 # renovate: datasource=nuget depName=dotnet-reportgenerator-globaltool
-$dotnetReportgeneratorGlobaltoolVersion = '5.5.1'
+$dotnetReportgeneratorGlobaltoolVersion = '5.5.3'
 dotnet tool install --global dotnet-reportgenerator-globaltool --version $dotnetReportgeneratorGlobaltoolVersion
