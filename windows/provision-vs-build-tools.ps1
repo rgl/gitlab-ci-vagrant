e00fb96acf0beb10779762c85e33c3951de7d42c
Rui Lopes <rgl@ruilopes.com>
2026-05-04 07:00:04 +0000

    upgrade to visual studio build tools 18.5.2

--- windows/provision-vs-build-tools.ps1
+++ windows/provision-vs-build-tools.ps1
@@ -1,7 +1,7 @@
 # add support for building applications that target the .net framework 4.8.1.
 choco install -y netfx-4.8.1-devpack
 
-# install the Visual Studio Build Tools 2026 18.4.1.
+# install the Visual Studio Build Tools 2026 18.5.2.
 # see https://learn.microsoft.com/en-us/visualstudio/releases/2026/release-history#release-dates-and-build-numbers
 # see https://learn.microsoft.com/en-us/visualstudio/releases/2026/release-notes
 # see https://learn.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=visualstudio
@@ -9,8 +9,8 @@ choco install -y netfx-4.8.1-devpack
 # see https://learn.microsoft.com/en-us/visualstudio/install/workload-and-component-ids?view=visualstudio
 # see https://learn.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools?view=visualstudio
 # NB update the windbg version in provision-procdump-as-postmortem-debugger.ps1 to match the installed Windows11SDK.26100.
-$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/691c58af-be53-40df-9b3e-d4fb7c24879f/c3bd55876027aa4caed9f957bd9ba9d07b28a8ee581a73f0066cae68d3f18ddb/vs_BuildTools.exe'
-$archiveHash = 'c3bd55876027aa4caed9f957bd9ba9d07b28a8ee581a73f0066cae68d3f18ddb'
+$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/ef8a1da5-1f31-4995-8be6-cb9800f90516/97b2740209702f310a53d1e92ce971e86aafbde916a4d6cd087464e98ff61e2b/vs_BuildTools.exe'
+$archiveHash = '97b2740209702f310a53d1e92ce971e86aafbde916a4d6cd087464e98ff61e2b'
 $archiveName = Split-Path $archiveUrl -Leaf
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host 'Downloading the Visual Studio Build Tools Setup Bootstrapper...'
